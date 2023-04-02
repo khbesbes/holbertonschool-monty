a3d6470204c1ebb0b94375fbf616a1fabd1a64d7
khbesbes <5786@holbertonstudents.com>
2023-04-02 02:11:49 +0100

    monty.h

---
+++ monty.h
@@ -0,0 +1,59 @@
+#ifndef _MONTY_H
+#define _MONTY_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <ctype.h>
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+int sum_tokens(char *, char *);
+size_t print(const stack_t *stack);
+stack_t *add_end(stack_t **stack, const int n);
+stack_t *add_begin(stack_t **satck, const int n);
+void swap_previous(stack_t *ptr);
+void add_previous(stack_t **ptr);
+void print_previous(const stack_t *kao);
+void pop_previous(stack_t **ptr);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int n);
+void (*get(char *b, unsigned int))(stack_t **, unsigned int);
+int free_list(char **list);
+void free_dlist(stack_t *ptr);
+
+#endif
